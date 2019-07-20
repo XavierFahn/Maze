2f465062fb912ba2ecd3a3277473a844e4c357d9
Xav <Xav@Xaviers-MacBook-Air.local>
2019-07-20 10:52:34 -0700

    README is now outline ideas

--- MazeBuilder.java
+++ MazeBuilder.java
@@ -1,3 +1,7 @@
+
+//How to print colored text in a console
+//https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+
 public class MazeBuilder {
     public static void main(String[] args) {
         System.out.print("test");
